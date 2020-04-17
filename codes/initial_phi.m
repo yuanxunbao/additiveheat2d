ea1b47293e5d086f929324e590c7249707bbdc93
Yuanxun Bao <yuanxun@Sysnets-MacBook-Pro.local>
2020-04-16 23:01:54 -0500

    modify initial conditoin

--- codes/initial_phi.m
+++ codes/initial_phi.m
@@ -15,12 +15,27 @@ z = 0:dy:Lz;
 w = 2e-2;
 z0 = 1e-1;
 n = 10;
-phi = -tanh((zz-z0-0.01*sin(2*pi/Lx*xx*n))/w);
+phi0 = -tanh((zz-z0-0.01*sin(2*pi/Lx*xx*n))/w);
+
+k = 0.3;
+U0 = 1/(1-k) * ( k./((1-phi0)/2+k*(1+phi0)/2) - 1);
 
 figure(1);
-surf(xx,zz,phi);
+subplot(121)
+surf(xx,zz,phi0);
 shading interp
-view(2); 
+view(2);
+colorbar
+title('phi')
+
+subplot(122) 
+surf(xx,zz,U0);
+shading interp
+view(2);
+colorbar
+title('U')
+
+
 
 print('-dpng','../tex/figures/phi_initial.png')
 
