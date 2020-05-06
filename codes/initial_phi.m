ffc1ecc1ab442051d1fd72223de30365c97e5b0f
Yuanxun Bao <yuanxun@Sysnets-MacBook-Pro.local>
2020-05-06 18:05:40 -0500

    modify initial

--- codes/initial_phi.m
+++ codes/initial_phi.m
@@ -8,34 +8,25 @@ dx = Lx/nx;
 dz = Lz/nz;
 
 x = 0:dx:Lx;
-z = 0:dy:Lz;
+z = 0:dz:Lz;
 
 [zz,xx] = meshgrid(z,x);
 
 w = 2e-2;
-z0 = 1e-1;
-n = 10;
-phi0 = -tanh((zz-z0-0.01*sin(2*pi/Lx*xx*n))/w);
+z0 = 2e-1;
+n = 1;
+phi0 = -tanh((zz-z0-0.01*cos(2*pi/Lx*xx*n) )/w);
 
-k = 0.3;
-U0 = 1/(1-k) * ( k./((1-phi0)/2+k*(1+phi0)/2) - 1);
 
 figure(1);
-subplot(121)
 surf(xx,zz,phi0);
 shading interp
 view(2);
 colorbar
 title('phi')
 
-subplot(122) 
-surf(xx,zz,U0);
-shading interp
-view(2);
-colorbar
-title('U')
 
 
 
-print('-dpng','../tex/figures/phi_initial.png')
+% print('-dpng','../tex/figures/phi_initial.png')
 
