538eacedabd65fd3bd486b0c3742f71623381e9f
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2020-05-15 16:26:34 -0500

    addnew results

--- codes/initial_phi.m
+++ codes/initial_phi.m
@@ -17,15 +17,21 @@ z0 = 2e-1;
 n = 1;
 phi0 = -tanh((zz-z0-0.01*cos(2*pi/Lx*xx*n) )/w);
 
+U0 = 0*phi0-1;
 
 figure(1);
+subplot(121);
 surf(xx,zz,phi0);
 shading interp
 view(2);
 colorbar
-title('phi')
-
-
+title('phi0')
+subplot(122)
+surf(xx,zz,U0);
+shading interp
+view(2);
+colorbar
+title('U0')
 
 
 % print('-dpng','../tex/figures/phi_initial.png')
