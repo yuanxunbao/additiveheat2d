1e8eb1dad5910f58c621719779395377aeacb2ee
Yigong Qin <yigongqin@Yigongs-MBP.lan1>
2020-07-04 17:08:53 -0500

    single precision

--- codes/dirsolid_single.py
+++ codes/dirsolid_single.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 25 23:10:13 2020
+Single precision: there should be no int/float64 in calculation
 
 @author: yigong qin, yuanxun bao
 """
@@ -11,11 +12,11 @@ import os
 from scipy.io import savemat as save
 from numba import njit, stencil, vectorize, float32, float64
 import numpy as np
-from numpy.random import random
+from numpy.random import random,rand
 import time
 from math import pi
 #PARA = importlib.import_module(sys.argv[1])
-import dsinput as PARA
+import dsinput_single as PARA
 from numpy import float32 as f32
 
 pi = f32(pi)
@@ -60,55 +61,8 @@ dt_sr = np.sqrt(dt)
 Tishot = np.zeros((2*nv,nts+1),dtype=f32)
 
 np.random.seed(seed)
+eps2 = np.sqrt(eps)
 
-@njit
-def set_halo(u):
-    
-    m,n = u.shape
-    ub = np.zeros((m+2,n+2),dtype=f32)
-    
-    ub[1:-1,1:-1] = u
-    
-    return ub
-
-
-
-@vectorize([float32(float32, float32),
-            float64(float64, float64)])
-def atheta(ux, uz):
-
-    ux2 = ( cosa*ux + sina*uz )**2
-    uz2 = ( -sina*ux + cosa*uz)**2
-        
-    # return MAG_sq2
-    MAG_sq2 = (ux2 + uz2)**2
-    
-    if (MAG_sq2 > eps**2):
-        
-        return a_s*( s1 + epsilon*(ux2**2 + uz2**2) / MAG_sq2   )
-        # return uz/MAG_sq2
-    else:
-        return a_s
-    
-    
-@vectorize([float32(float32, float32),
-            float64(float64, float64)])
-def aptheta(ux, uz):
-    uxr = cosa*ux + sina*uz
-    uzr = -sina*ux + cosa*uz
-    ux2 = uxr**2
-    uz2 = uzr**2
-    
-    MAG_sq2 = (ux2 + uz2)**2
-    
-    if (MAG_sq2 > eps**2):
-        
-        return -a_12*uxr*uzr*(ux2 - uz2) /  MAG_sq2
-    
-    else:
-        return s0
-    
-    
 # update global array, don't jit compile this function
 def set_BC(u,BCx,BCy):
     
@@ -136,12 +90,66 @@ def set_BC(u,BCx,BCy):
         
         
     return u
+
+@njit
+def set_halo(u):
+    
+    m,n = u.shape
+    ub = np.zeros((m+2,n+2),dtype=f32)
+    
+    ub[1:-1,1:-1] = u
+    
+    return ub
+
+
+
+@vectorize([float32(float32, float32)])
+def atheta(ux, uz):
+
+    ux2 = ( cosa*ux + sina*uz )**s2
+    uz2 = ( -sina*ux + cosa*uz)**s2
         
+    # return MAG_sq2
+    MAG_sq2 = (ux2 + uz2)**s2
+    
+    if (MAG_sq2 > eps**s2):
+        
+        return a_s*( s1 + epsilon*(ux2**s2 + uz2**s2) / MAG_sq2   )
+        
+    else:
+        return a_s
+    
+    
+@vectorize([float32(float32, float32)])
+def aptheta(ux, uz):
+    uxr = cosa*ux + sina*uz
+    uzr = -sina*ux + cosa*uz
+    ux2 = uxr**s2
+    uz2 = uzr**s2
+    
+    MAG_sq2 = (ux2 + uz2)**s2
+    
+    if (MAG_sq2 > eps**s2):
+        
+        return -a_12*uxr*uzr*(ux2 - uz2) /  MAG_sq2
+    
+    else:
+        return s0
+
+
+
+
+@vectorize([float32(float32, float32, float32, float32)])
+def divi(a, b, ep, bound):    
+    nx = a / b if (b > ep) else bound    
+    return nx
+
+
         
 
 @stencil
 def _rhs_psi(ps,ph,U,zz):
-
+    #'''
     # ps = psi, ph = phi
     
     # =============================================================
@@ -160,7 +168,7 @@ def _rhs_psi(ps,ph,U,zz):
     phipjm=( ph[ 1, 0] + ph[ 0, 0] + ph[ 0,-1] + ph[ 1,-1] ) * p25
     phimjp=( ph[ 0, 1] + ph[-1, 1] + ph[-1, 0] + ph[ 0, 0] ) * p25
     phimjm=( ph[ 0, 0] + ph[-1, 0] + ph[-1,-1] + ph[ 0,-1] ) * p25
-    
+
     # ============================
     # right edge flux
     # ============================
@@ -210,8 +218,7 @@ def _rhs_psi(ps,ph,U,zz):
     Ap = aptheta(phx,phz)
     JB = A * ( A*psz + Ap*psx )
     
-    
-    
+
     # =============================================================
     # 
     # 2. EXTRA TERM: sqrt2 * atheta**2 * phi * |grad psi|^2
@@ -224,11 +231,11 @@ def _rhs_psi(ps,ph,U,zz):
     psxn = ( ps[ 1, 0] - ps[-1, 0] ) * p5
     pszn = ( ps[ 0, 1] - ps[ 0,-1] ) * p5
     
-    A2 = atheta(phxn,phzn)**2
-    gradps2 = (psxn)**2 + (pszn)**2
+    A2 = atheta(phxn,phzn)**s2
+    gradps2 = (psxn)**s2 + (pszn)**s2
     extra =  -sqrt2 * A2 * ph[0,0] * gradps2
     
-
+    
     # =============================================================
     # 
     # 3. double well (transformed): sqrt2 * phi + nonlinear terms
@@ -237,8 +244,8 @@ def _rhs_psi(ps,ph,U,zz):
     
     Up = (zz[0,0] )/lT_tilde
     
-    rhs_psi = ((JR-JL) + (JT-JB) + extra) * hi**2 + \
-               sqrt2*ph[0,0] - lamd*(s1-ph[0,0]**2)*sqrt2*(U[0,0] + Up) 
+    rhs_psi = ((JR-JL) + (JT-JB) + extra) * hi**s2 + \
+             sqrt2*ph[0,0] - lamd*(s1-ph[0,0]**s2)*sqrt2*(U[0,0] + Up) 
         
     
     # =============================================================
@@ -248,14 +255,14 @@ def _rhs_psi(ps,ph,U,zz):
     # =============================================================
     tp = (s1-(s1-k)*Up)
     tau_psi = tp*A2 if tp >= k else k*A2
-
-    return rhs_psi/tau_psi + eta*(f32(random())- p5 )/dt_sr
+    
+    return divi(rhs_psi, tau_psi,s0, s0) #+ eta*(f32(random())- p5 )/dt_sr
 
 
 
 
 @stencil
-def _rhs_U(U,ph,psi_t):
+def _rhs_U(U,ph,jat):
     
     # define cell centered values
     phipjp=( ph[ 1, 1] + ph[ 0, 1] + ph[ 0, 0] + ph[ 1, 0] ) * p25
@@ -268,42 +275,43 @@ def _rhs_U(U,ph,psi_t):
     # ============================
     phx = ph[1,0]-ph[0,0]
     phz = phipjp - phipjm
-    phn2 = phx**2 + phz**2
-    nx = phx / np.sqrt(phn2) if (phn2 > eps) else s0
+    phn = np.sqrt(phx**s2 + phz**s2) 
+    nx = divi(phx, phn, eps2, s0)
     
-    jat    = p5*(s1+(s1-k)*U[0,0])*(s1-ph[0,0]**2)*psi_t[0,0]
-    jat_ip = p5*(s1+(s1-k)*U[1,0])*(s1-ph[1,0]**2)*psi_t[1,0]
+    
+    #jat    = p5*(s1+(s1-k)*U[0,0])*(s1-ph[0,0]**s2)*psi_t[0,0]
+    #jat_ip = p5*(s1+(s1-k)*U[1,0])*(s1-ph[1,0]**s2)*psi_t[1,0]
         
     UR = hi*Dl_tilde*p5*(s2 - ph[0,0] - ph[1,0])*(U[1,0]-U[0,0]) + \
-         p5*(jat + jat_ip)*nx
+         p5*(jat[0,0] + jat[1,0])*nx
          
-         
+       
     # ============================
     # left edge flux (i-1/2, j)
     # ============================
     phx = ph[0,0]-ph[-1,0]
     phz = phimjp - phimjm
-    phn2 = phx**2 + phz**2
-    nx = phx / np.sqrt(phn2) if (phn2 > eps) else s0
+    phn = np.sqrt(phx**s2 + phz**s2) 
+    nx = divi(phx, phn, eps2, s0)
     
-    jat_im = p5*(s1+(s1-k)*U[-1,0])*(s1-ph[-1,0]**2)*psi_t[-1,0]
-    
+    #jat_im = p5*(s1+(s1-k)*U[-1,0])*(s1-ph[-1,0]**s2)*psi_t[-1,0]
+
     UL = hi*Dl_tilde*p5*(s2 - ph[0,0] - ph[-1,0])*(U[0,0]-U[-1,0]) + \
-         p5*(jat + jat_im)*nx
+         p5*(jat[0,0] + jat[-1,0])*nx
          
-         
+     
     # ============================
     # top edge flux (i, j+1/2)
     # ============================     
     phx = phipjp - phimjp
     phz = ph[0,1]-ph[0,0]
-    phn2 = phx**2 + phz**2
-    nz = phz / np.sqrt(phn2) if (phn2 > eps) else s0
+    phn = np.sqrt(phx**s2 + phz**s2) 
+    nz = divi(phz, phn, eps2, s0)
           
-    jat_jp = p5*(s1+(s1-k)*U[0,1])*(s1-ph[0,1]**2)*psi_t[0,1]      
+    #jat_jp = p5*(s1+(s1-k)*U[0,1])*(s1-ph[0,1]**s2)*psi_t[0,1]      
     
     UT = hi*Dl_tilde*p5*(s2 - ph[0,0] - ph[0,1])*(U[0,1]-U[0,0]) + \
-         p5*(jat + jat_jp)*nz
+         p5*(jat[0,0] + jat[0,1])*nz
          
          
     # ============================
@@ -311,26 +319,27 @@ def _rhs_U(U,ph,psi_t):
     # ============================  
     phx = phipjm - phimjm
     phz = ph[0,0]-ph[0,-1]
-    phn2 = phx**2 + phz**2
-    nz = phz / np.sqrt(phn2) if (phn2 > eps) else s0
+    phn = np.sqrt(phx**s2 + phz**s2) 
+    nz = divi(phz, phn, eps2, s0)
     
-    jat_jm = p5*(s1+(s1-k)*U[0,-1])*(s1-ph[0,-1]**2)*psi_t[0,-1]      
+    #jat_jm = p5*(s1+(s1-k)*U[0,-1])*(s1-ph[0,-1]**s2)*psi_t[0,-1]      
     
     UB = hi*Dl_tilde*p5*(s2 - ph[0,0] - ph[0,-1])*(U[0,0]-U[0,-1]) + \
-         p5*(jat + jat_jm)*nz
+         p5*(jat[0,0] + jat[0,-1])*nz
     
-    rhs_U = ( (UR-UL) + (UT-UB) ) * hi + sqrt2 * jat
+    rhs_U = ( (UR-UL) + (UT-UB) ) * hi + sqrt2 * jat[0,0]
     tau_U = (s1+k) - (s1-k)*ph[0,0]
     
-    
+   
     return rhs_U/tau_U
     
 
-@njit(parallel=True)
+
+@njit#(parallel=True)
 def rhs_psi(ps,ph,U,zz): return _rhs_psi(ps,ph,U,zz)
 
 
-@njit(parallel=True)
+@njit#(parallel=True)
 def rhs_U(U,ph,psi_t): return _rhs_U(U,ph,psi_t)
 
 
@@ -352,16 +361,18 @@ psi = set_halo(psi0.T)
 U = set_halo(U0.T)
 zz = set_halo(zz.T)
 
+
 psi = set_BC(psi, 0, 1)
 phi = np.tanh(psi/sqrt2)   # expensive replace
 U =   set_BC(U, 0, 1)
 Tishot[:,[0]] = save_data(phi,U)
 
+psi6 = np.float64(psi)
+
 
 #complie
 start = time.time()
-dPSI = rhs_psi(psi, phi, U, zz)
-dPSI = set_BC(dPSI, 0, 1)
+dPSI = rhs_psi(psi, phi, U, zz - R_tilde*t)
 dU = rhs_U(U,phi,dPSI)
 
 end = time.time()
@@ -372,18 +383,23 @@ print('elapsed: ', end - start )
 
 start = time.time()
 
+
+
+
 for jj in range(nts):
 
     for ii in range(int(Mt/nts)):
-
-
-        dPSI = rhs_psi(psi, phi, U, zz - R_tilde*t)
-    
-        dPSI = set_BC(dPSI, 0, 1)
         
-        psi = psi + dt*dPSI 
-      
-        U = U + dt*rhs_U(U,phi,dPSI)
+        #print(psi.dtype,phi.dtype,U.dtype,(zz - R_tilde*t).dtype)
+        dPSI = rhs_psi(psi, phi, U, zz - R_tilde*t)
+        
+        
+        jat = set_BC( p5*(s1+(s1-k)*U)*(s1-phi**s2)*dPSI , 0, 1)
+        
+        psi = psi + dt*dPSI #+ dt_sr*eta*f32(rand(nx+2,nz+2)-p5)
+       
+        U = U + dt*rhs_U(U, phi, jat )
+       
         
         # add boundary
         psi = set_BC(psi, 0, 1)
@@ -397,7 +413,7 @@ for jj in range(nts):
 
     #print('now time is ',t)  
     Tishot[:,[jj+1]] = save_data(phi,U)
-    
+
 end = time.time()
 
 
@@ -406,4 +422,4 @@ print('elapsed: ', end - start )
 
 Uf = U[1:-1,1:-1].T
 
-save(os.path.join(direc,filename),{'xx':xx*W0,'zz':zz[1:-1,1:-1].T*W0,'y':Tishot,'dt':dt*tau0,'nx':nx,'nz':nz,'t':t*tau0,'mach_time':end-start})
+#save(os.path.join(direc,filename),{'xx':xx*W0,'zz':zz[1:-1,1:-1].T*W0,'y':Tishot,'dt':dt*tau0,'nx':nx,'nz':nz,'t':t*tau0,'mach_time':end-start})
