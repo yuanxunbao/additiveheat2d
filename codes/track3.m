dc8db176fdfc8c136547336f987de908e3bf2c1b
Yuanxun Bao <yuanxun@Sysnets-MBP.attlocal.net>
2020-09-28 11:40:47 -0500

    modify plot

--- codes/track3.m
+++ codes/track3.m
@@ -6,8 +6,12 @@ savedir = './';
 
 load macro_output_low.mat
 
-idx = [10:20:100]; % traj index
-t_idx = [1:70]; % time index
+sz = size(G_traj);
+ntraj = sz(1); 
+ntime = sz(2);
+
+idx = [5:10: ntraj/2-5]; % traj index
+t_idx = [1: ntime-10]; % time index
 
 
 figure(1);
@@ -15,17 +19,71 @@ set(gcf,'Position',[100,100,800,400])
 for k = 1:length(idx)
     plot(x_traj(t_idx ,idx(k)), y_traj( t_idx, idx(k)),'o-','MarkerSize',3) ; hold on
 end
-legend({'traj 1','traj 2', 'traj 3', 'traj 4'},'location','se')
+legend({'traj 1','traj 2', 'traj 3', 'traj 4', 'traj 5'},'location','se')
 
+
+% plot contours
+contour_lvl = [ 1:10: ntime-10];
+for j = 1:length(contour_lvl)
+    h1=plot(x_traj(contour_lvl(j),: ), y_traj( contour_lvl(j), :),'k--'); hold on;
+    h1.Annotation.LegendInformation.IconDisplayStyle = 'off'; % to turn of legend off
+    if j == 1 || j==length(contour_lvl)
+        text(x_traj(contour_lvl(j), end), y_traj(contour_lvl(j), end),sprintf('t = %.2f', time_traj(contour_lvl(j)) ))
+    end
+end
+axis equal;
+axis([7e-3 11e-3 -2e-3 0]);
+xlabel('x')
+ylabel('y')
+
+print('-depsc', sprintf('%s/macro_traj.eps',savedir), '-r300' )
+
+
+
+% plot G, R, Tdot
+t_idx = [1:70]; % time index
 figure(2);
-subplot(121)
+set(gcf,'Position',[100,100,1200,400])
+subplot(131)
 for k = 1:length(idx)
     plot(time_traj(t_idx), G_traj(t_idx ,idx(k)),'-') ; hold on
 end
 title('G(t)')
+xlabel('t')
 
-subplot(122)
+subplot(132)
 for k = 1:length(idx)
     plot(time_traj(t_idx), R_traj(t_idx ,idx(k)),'-') ; hold on
 end
 title('R(t)')
+xlabel('t')
+
+% cooling rate
+subplot(133)
+Tdot = - G_traj .* R_traj;
+for k = 1:length(idx)
+    plot(time_traj(t_idx), Tdot(t_idx ,idx(k)) ,'-') ; hold on
+end
+title('cooling rate dT/dt')
+xlabel('t')
+
+print('-depsc', sprintf('%s/macro_GR.eps',savedir), '-r300' )
+
+
+% save data
+savedir = './';
+t_list = 1:60;
+for k = 1:length(idx)
+    
+    t_macro = time_traj(t_list);
+    G_t = G_traj(t_list,idx(k))' / 1e6; % convert from K/m to K/um
+    R_t = R_traj(t_list,idx(k))' * 1e6; % convert from m/s to um/s
+    R_t(1) = 0;
+   
+    
+    save(sprintf('%s/macroGR_traj%d.mat',savedir, k), 'G_t', 'R_t', 't_macro');
+end
+
+
+
+
